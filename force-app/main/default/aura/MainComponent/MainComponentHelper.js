41b1b31f7024f936795b54eb5571ad719155ec21
Dinh <thinh.dinh@capgemini.com>
2020-04-22 15:48:08 +0300

    Added static resource and styling

--- force-app/main/default/aura/MainComponent/MainComponentHelper.js
+++ force-app/main/default/aura/MainComponent/MainComponentHelper.js
@@ -10,12 +10,18 @@
                 var state = res.getState();
                 if(com.isValid() && state === 'SUCCESS'){
                     console.log("New account successfully created!");
+                    document.getElementById("mainContainer").style.display = 'none';
+                    document.getElementById("thankYouAlert").style.display = 'block';
+                    setTimeout(function(){
+                        window.location.href = "https://www.telia.fi/";
+                    }, 3000);
                 } else if(state === 'ERROR'){
                     var errors = res.getError();
                     console.log(JSON.stringify(errors));
                     if(errors){
-                        if(errors[0] && errors[0].message){
-                            console.log("Error while processing: "+ errors[0].message);
+                        if(errors[0] && errors[0].pageErrors && errors[0].pageErrors[0].statusCode === 'DUPLICATE_VALUE'){
+                            console.log("Error while processing: "+ errors[0].pageErrors[0].message);
+                            document.getElementById("alertWarningDiv").style.display = 'block';
                         }
                     }
                 } else {
